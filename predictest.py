437e061ab6a658859a3ea831c915bb69f39b44ee
Sundar Narasiman <narasund@amazon.com>
2018-02-23 13:44:29 +0530

    first commit

---
+++ predictest.py
@@ -0,0 +1,6 @@
+import inception_predict
+
+#inception_predict.predict_from_local_file('pen.jpg')
+#inception_predict.predict_from_url('https://imgur.com/HzafyBA')
+print('test')
+inception_predict.predictfromcamera()
